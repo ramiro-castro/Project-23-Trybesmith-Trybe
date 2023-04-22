a98baac3342d589809b786e4c02ad8638a1e883a
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2023-04-22 19:54:28 -0300

    refatorando e limpando o codigo de orders.controllers.ts

--- src/controllers/orders.controllers.ts
+++ src/controllers/orders.controllers.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-// import Jwt from 'jsonwebtoken';
 import { UserJwt } from '../interfaces/all.interfaces';
 import validationsInputValues from '../middlewares/validationsInputValues';
 import OrdersServices from '../services/orders.services';
@@ -21,14 +20,13 @@ const OrdersControllers = {
     const { productsIds } = req.body;
 
     const token = req.headers.authorization as string;
-    if (!token) return res.status(401).json({ message: 'Token not found' });
-    const decoded = await validationsInputValues.decryptToken(token);
-    console.log(decoded);
+    if (!token) return res.status(statusCodes.UNAUTHORIZED).json({ message: 'Token not found' });
     
-    if (decoded === null) return res.status(401).json({ message: 'Invalid token' });
-
+    const decoded = await validationsInputValues.decryptToken(token);
+    if (decoded === null) { 
+      return res.status(statusCodes.UNAUTHORIZED).json({ message: 'Invalid token' });
+    }
     const checkProductsIds = validationsInputValues.validateProductsIds(productsIds);
-    //   console.log(checkProductsIds); 
     if (checkProductsIds.type) {
       res.status(checkProductsIds.type).json({ message: checkProductsIds.message });
       return;
@@ -36,11 +34,9 @@ const OrdersControllers = {
 
     const { payload } = decoded as unknown as UserJwt;
     const { id } = payload;
-    // console.log(id);
     const message = { userId: id, productsIds };
-    //   console.log(message);
     await OrdersServices.create(Number(id), productsIds);
-    res.status(201).json(message);
+    res.status(statusCodes.CREATED).json(message);
   },
   
 };
